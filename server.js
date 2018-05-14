9cf7a72af13383851846f050cb488a4864389589
m67hoff <mhoff@web.de>
2018-05-14 18:30:24 +0200

    config

--- server.js
+++ server.js
@@ -5,38 +5,41 @@ const fs = require('fs')
 const path = require('path')
 const express = require('express')
 const app = express()
-const clientconfig = require('./clientconfig.json')
+
+
+const CLIENTCONFIG = './clientconfig.json'
 
 
 // const CONFIG = path.join(__dirname, './serverconfig.json') 
 const CONFIG = './serverconfig.json'
 var PORT = 8080
+var LOGLEVEL = 'warn'
 
 function json2s (obj) { return JSON.stringify(obj, null, 2) }  // format JSON payload for log
 
 function loadConf () {
   var c = JSON.parse(fs.readFileSync(CONFIG))
-  if (c.LOGLEVEL) { log.level = c.LOGLEVEL }
-  console.log('log  ', 'Read Config - Set LOGLEVEL to %j', c.LOGLEVEL)
+  if (c.LOGLEVEL) { LOGLEVEL = c.LOGLEVEL }
+  console.log('Read Config - Set LOGLEVEL to %j', c.LOGLEVEL)
   if (c.PORT) { PORT = c.PORT }
   return c
 }
 
-console.log('static_path:', path.join(__dirname, '/webclient'))
-
 loadConf()
-
 
 if (process.env.VCAP_APP_PORT) { PORT = process.env.VCAP_APP_PORT }
 app.listen(PORT, function () {
   console.log('express', 'server starting on ' + PORT)
 })
 
+console.log('static_path:', path.join(__dirname, '/webclient'))
 app.use(express.static(path.join(__dirname, '/webclient')))
 
 app.get('/config', (req, res) => {
   console.log('NodeRequest ' + req.method + ' ' + req.originalUrl)
-  res.send(clientconfig)
+  var out = JSON.parse(fs.readFileSync(CLIENTCONFIG))
+  console.log('env:', json2s(out))
+  res.send(out)
 })
 
 app.get('/info', (req, res) => {
